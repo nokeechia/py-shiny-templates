1afa69d9bd359ed288d6ba53ef95e742f930ced7
nokeechia <nokeechia@gmail.com>
2024-04-29 07:58:45 +0200

    Update app-core.py to include ipydatagrid table and functionality

--- survey/app-core.py
+++ survey/app-core.py
@@ -1,31 +1,39 @@
 from pathlib import Path
-
+from ipydatagrid import DataGrid
 import pandas as pd
 from shared import INPUTS
-from shiny import App, Inputs, Outputs, Session, reactive, ui
+from shiny import App, Inputs, Outputs, Session, reactive, ui, render
 from shiny_validate import InputValidator, check
+from shinywidgets import output_widget, register_widget
 
 app_dir = Path(__file__).parent
 
-app_ui = ui.page_fixed(
+app_ui = ui.page_fluid(
     ui.include_css(app_dir / "styles.css"),
     ui.panel_title("Movie survey"),
+    # ui.card(
+    #     ui.card_header("Demographics"),
+    #     INPUTS["name"],
+    #     INPUTS["country"],
+    #     INPUTS["age"],
+    # ),
+    # ui.card(
+    #     ui.card_header("Income"),
+    #     INPUTS["income"],
+    # ),
+    # ui.card(
+    #     ui.card_header("Ratings"),
+    #     INPUTS["avengers"],
+    #     INPUTS["spotlight"],
+    #     INPUTS["the_big_short"],
+    # ),
+
     ui.card(
-        ui.card_header("Demographics"),
-        INPUTS["name"],
-        INPUTS["country"],
-        INPUTS["age"],
-    ),
-    ui.card(
-        ui.card_header("Income"),
-        INPUTS["income"],
-    ),
-    ui.card(
-        ui.card_header("Ratings"),
-        INPUTS["avengers"],
-        INPUTS["spotlight"],
-        INPUTS["the_big_short"],
-    ),
+            output_widget("table"),
+            # ui.card_header("Penguin data"),
+            # ui.output_data_frame("summary_statistics"),
+            full_screen=True,
+        ),
     ui.div(
         ui.input_action_button("submit", "Submit", class_="btn btn-primary"),
         class_="d-flex justify-content-end",
@@ -56,6 +64,54 @@ def server(input: Inputs, output: Outputs, session: Session):
             df.to_csv(responses, mode="a", header=False)
 
         ui.modal_show(ui.modal("Form submitted, thank you!"))
+
+    def read_responses():
+        return pd.read_csv(app_dir / "responses.csv")
+
+    def view_responses():
+        return read_responses()
+
+    tbl = DataGrid(read_responses()[[
+        "name", "country", "age", "income","avengers","spotlight","the_big_short"]],
+        editable=True, auto_fit_columns=True, index_name="index")
+    register_widget("table", tbl)
+
+    # Create a reactive value for tracking cell changes
+    cell_changes = reactive.Value()
+
+    cell_selection = reactive.Value()
+
+    def on_cell_changed(cell):
+        cell_changes.set(str(cell))
+
+    def on_cell_selected(cell):
+        cell_selection.set(str(cell))
+
+    # register callback
+    tbl.on_cell_change(on_cell_changed)
+
+    # Print change, just to demonstrate
+    @reactive.Effect
+    def print_change():
+        print(eval(cell_changes()))
+
+    @reactive.Effect
+    def print_selection():
+        print(eval(cell_selection()))
+
+    @reactive.Effect
+    def save_cell_changes():
+        # Save changes to csv
+        df = read_responses()
+        cell = eval(cell_changes())
+        df.loc[cell["row"], cell["column"]] = cell["value"]
+        clean_df = df
+        # df.drop(columns=["Unnamed: 0"], inplace=True)
+        clean_df.to_csv(app_dir / "responses.csv", mode="w", header=True, index=False)
+
+    # @render.data_frame
+    # def summary_statistics():
+    #     return render.DataGrid(read_responses(), filters=True)
 
 
 app = App(app_ui, server)
